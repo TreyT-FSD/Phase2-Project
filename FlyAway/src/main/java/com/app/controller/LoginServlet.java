1e832f1f96875a6ee187d11aecde44f0b2a24b3e
treyturley <81778542+treyturley@users.noreply.github.com>
2021-06-12 09:31:51 -0500

    initial commit

---
+++ FlyAway/src/main/java/com/app/controller/LoginServlet.java
@@ -0,0 +1,63 @@
+package com.app.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(password.compareTo("pass") == 0) {
+			
+			HttpSession session = request.getSession();
+			
+			session.setAttribute("username", username);
+			session.setAttribute("authenticated", "True");
+			
+			request.getRequestDispatcher("admin.jsp").forward(request, response);
+			
+		}
+		else {
+			request.getRequestDispatcher("login.jsp").include(request, response);
+			
+			out.print("Invalid User");
+			
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
---
+++ FlyAway/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql" %>
+
+<html>
+	<head>
+		<title>FlyAway - Homepage</title>
+	</head>
+	<body>
+		<%@ include file="nav.html" %>
+		<h1>Homepage</h1>
+		search criteria will go here
+	</body>
+</html>
